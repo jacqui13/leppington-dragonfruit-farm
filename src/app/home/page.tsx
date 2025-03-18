8350e401db19be7f6bfa69dad111a2086c306fc7
jacqui13 <jacquelyn.huang@hotmail.com>
2025-03-19 00:48:09 +1100

    Minor changes to home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,9 +4,9 @@ import { Container } from "react-bootstrap";
 
 export default function Home() {
     return (
-        <Container style={{ margin: "0" }}>
+        <Container style={{ margin: "0", padding: "0" }}>
             <h1 style={{ justifySelf: "anchor-center", alignSelf: "anchor-center", background: "pink", position: "absolute" }}>Welcome to Leppington Dragonfruit Farm</h1>
-            <Image src="/IMG_9185.jpg" alt="dragonfruits" width={1000} height={800} />
+            <Image src="/IMG_9185.jpg" alt="dragonfruits" width={1000} height={800} style={{ width: "100vw", height: "130vh" }} />
         </Container>
     )
 }
