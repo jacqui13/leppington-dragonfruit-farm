3ca5aea31e6380aef3cda2969d24b096262eaf07
jacqui13 <jacquelyn.huang@hotmail.com>
2025-03-19 00:42:31 +1100

    Add image and title to home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,12 @@
 "use client"
+import Image from "next/image";
 import { Container } from "react-bootstrap";
 
 export default function Home() {
     return (
-        <Container>
-            Home
+        <Container style={{ margin: "0" }}>
+            <h1 style={{ justifySelf: "anchor-center", alignSelf: "anchor-center", background: "pink", position: "absolute" }}>Welcome to Leppington Dragonfruit Farm</h1>
+            <Image src="/IMG_9185.jpg" alt="dragonfruits" width={1000} height={800} />
         </Container>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Home from "./home/page";
+
 export default function Page() {
     return (
-        <div></div>
+        <Home></Home>
     );
 }
