a82739ffb9a6c1ef636e762f564ea54431184ffe
jacqui13 <jacquelyn.huang@hotmail.com>
2025-03-19 00:33:11 +1100

    Add responsive nav bar for mobile screen

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,19 +6,22 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
     return (
-        <Navbar bg="light" data-bs-theme="light" style={{ position: "sticky", zIndex: 1 }}>
+        <Navbar collapseOnSelect expand="lg" bg="light" data-bs-theme="light" style={{ position: "sticky", zIndex: 1 }}>
             <Container>
                 <Image src="/dragon_fruit.png" width={40} height={40} alt="dragonfruit emoji" />
                 <Navbar.Brand href="/home">
                     Leppington Dragonfruit Farm
                 </Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="/home">Home</Nav.Link>
-                    <Nav.Link href="/visit-us">Visit Us</Nav.Link>
-                    <Nav.Link href="/updates">Harvest Updates</Nav.Link>
-                    <Nav.Link href="/gallery">Gallery</Nav.Link>
-                    <Nav.Link href="/contact">Contact</Nav.Link>
-                </Nav>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Nav.Link href="/visit-us">Visit Us</Nav.Link>
+                        <Nav.Link href="/updates">Harvest Updates</Nav.Link>
+                        <Nav.Link href="/gallery">Gallery</Nav.Link>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
