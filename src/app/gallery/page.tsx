4eeb854ffb8d0e86bdea63571741b5a701d7a4df
jacqui13 <jacquelyn.huang@hotmail.com>
2025-03-19 00:13:44 +1100

    Using next/image component

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,47 +1,48 @@
 "use client"
-import { Carousel, Image } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
+import Image from 'next/image';
 
 export default function Gallery() {
     return (
         <Carousel style={{ height: "90vh", width: "60vw", justifySelf: "center" }}>
             <Carousel.Item>
-                <Image src="./IMG_9182.jpg" style={{ height: "100%", width: "100%" }} />
+                <Image src="./IMG_9182.jpg" style={{ height: "100%", width: "100%" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9184.jpg" style={{ height: "100%", width: "100%" }} />
+                <Image src="./IMG_9184.jpg" style={{ height: "100%", width: "100%" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9185.jpg" style={{ height: "100%", width: "100%" }} />
+                <Image src="./IMG_9185.jpg" style={{ height: "100%", width: "100%" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9191.JPG" style={{ height: "100%", width: "100%" }} />
+                <Image src="./IMG_9191.JPG" style={{ height: "100%", width: "100%" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9229.JPG" style={{ height: "100%", width: "100%", position: "relative", top: "-350px" }} />
+                <Image src="./IMG_9229.JPG" style={{ height: "100%", width: "100%", position: "relative", top: "-350px" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9237.PNG" style={{ height: "100%", width: "100%", position: "relative", top: "-300px" }} />
+                <Image src="./IMG_9237.PNG" style={{ height: "100%", width: "100%", position: "relative", top: "-300px" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9324.jpg" style={{ height: "100%", width: "100%" }} />
+                <Image src="./IMG_9324.jpg" style={{ height: "100%", width: "100%" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9348.jpg" style={{ height: "100%", width: "100%" }} />
+                <Image src="./IMG_9348.jpg" style={{ height: "100%", width: "100%" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9364.JPG" style={{ height: "100%", width: "100%" }} />
+                <Image src="./IMG_9364.JPG" style={{ height: "100%", width: "100%" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9517.jpg" style={{ height: "100%", width: "100%" }} />
+                <Image src="./IMG_9517.jpg" style={{ height: "100%", width: "100%" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9561.jpg" style={{ height: "100%", width: "100%", position: "relative", top: "-250px" }} />
+                <Image src="./IMG_9561.jpg" style={{ height: "100%", width: "100%", position: "relative", top: "-250px" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9562.jpg" style={{ height: "100%", width: "100%" }} />
+                <Image src="./IMG_9562.jpg" style={{ height: "100%", width: "100%" }} alt="dragonfruits" />
             </Carousel.Item>
             <Carousel.Item>
-                <Image src="./IMG_9564.png" style={{ height: "100%", width: "100%" }} />
+                <Image src="./IMG_9564.png" style={{ height: "100%", width: "100%" }} alt="dragonfruits" />
             </Carousel.Item>
         </Carousel>
     )
