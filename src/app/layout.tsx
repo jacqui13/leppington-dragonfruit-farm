674b5939a61591a136ff51eca93cbd1c152c460b
jacqui13 <jacquelyn.huang@hotmail.com>
2025-03-20 22:00:08 +1100

    Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
             <body className={`${geistSans.variable} ${geistMono.variable}`}>
                 <Header></Header>
                 {children}
+                <Analytics />
             </body>
         </html>
     );
