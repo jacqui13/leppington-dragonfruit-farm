06b3c328ba9112895c1ff43eba2674bb21b5f254
jacqui13 <jacquelyn.huang@hotmail.com>
2025-04-23 21:22:41 +1000

    Harvest updates

--- src/app/updates/page.tsx
+++ src/app/updates/page.tsx
@@ -9,10 +9,10 @@ export default function Updates() {
                 <Card.Body>
                     <p><strong>Great news, dragonfruit lovers! 🎉</strong></p>
                     <p>
-                        Our second round of dragonfruit harvest are available <strong>now.</strong>. 🍃✨
+                        Our second round of dragonfruit harvest are available <strong>now.</strong>. It will be available until sold out (estimated to be sold out beginning of May). 🍃✨
                     </p>
                     <p>
-                        We also have fresh peanuts, pumpkin and wintermelons in our shop, available now.
+                        We also have fresh peanuts, pumpkin and wintermelons in our shop, available now. <strong>Special - Buy 2kg peanuts and get 3kg wintermelon for free.</strong> Please <strong>text message</strong> 0432198893 for any further questions.
                     </p>
                     <p>
                         Stay tuned for more updates, and feel free to check back here for the latest availability.
